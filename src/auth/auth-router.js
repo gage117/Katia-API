e3b35cf50533cea760e439dadbe927497c4835c3
Ron Martin <ronmartin89@gmail.com>
2020-04-21 13:56:15 -0700

    C:/Program Files/Git/auth/token POST and PUT are working

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const AuthService = require('./auth-service')
 const { requireAuth } = require('../middleware/jwt-auth')
-// REQUIREAUTH - checkout
 
 const authRouter = express.Router()
 const jsonBodyParser = express.json()
