399f0d1f360165a05a09d75afe6e694ae3c8cec3
Jose <jozylopez27@gmail.com>
2020-04-28 14:39:17 -0500

    update user info

---
+++ migrations/007.undo.update_user_info_table.sql
@@ -0,0 +1,17 @@
+Alter TABLE user_info
+DROP COLUMN IF EXISTS psn;
+
+Alter TABLE user_info
+DROP COLUMN IF EXISTS xbox;
+
+Alter TABLE user_info
+DROP COLUMN IF EXISTS steam;
+
+Alter TABLE user_info
+DROP COLUMN IF EXISTS discord;
+
+Alter TABLE user_info
+DROP COLUMN IF EXISTS nintendo;
+
+Alter TABLE user_info
+DROP COLUMN IF EXISTS other;
