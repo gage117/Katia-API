cf4e853418b306b5c6e5bafa816558b790c7b46c
Ron Martin <ronmartin89@gmail.com>
2020-04-29 14:39:04 -0700

    trying to fix my async filter

--- src/matched/matched-router.js
+++ src/matched/matched-router.js
@@ -18,18 +18,8 @@ matchedRouter
         req.params.userId
       );
       console.log(possibleMatches);
-      console.log( possibleMatches[0].match_user_id);
-      console.log( possibleMatches[1].match_user_id);
-      console.log( possibleMatches[2].match_user_id);
       // checks each match our user has made and will see if a match exists
-      const test = await MatchedService.matchExists(
-          req.app.get('db'),
-          possibleMatches[1].match_user_id,
-          req.params.userId  
-      );
-
-      console.log(test);
-      const results2 = possibleMatches.filter(async (possibleMatch) => {
+      const matched = possibleMatches.filter(async possibleMatch => {
         const result = await MatchedService.matchExists(
             req.app.get('db'),
             possibleMatch.match_user_id,
@@ -38,9 +28,16 @@ matchedRouter
 
         console.log(`result: ${result} ${typeof result}`);
         return result;
+
+        // return await MatchedService.matchExists(
+        //     req.app.get('db'),
+        //     possibleMatch.match_user_id,
+        //     req.params.userId  
+        // );
       });
 
-      console.log(`results2:`, results2);
+      console.log(`matched:`, matched);
+      //return Promise.all(matched).then(matched => res.json(matched));
       res.json({possibleMatches});
 
     } catch (error) {
@@ -48,6 +45,29 @@ matchedRouter
     }
 
   });
+
+//   userRouter
+//   .route('/:userId/matches')
+//   .get(checkUserExists, (req, res, next) => {
+//     SwipeService.getUserMatches(
+//       req.app.get('db'),
+//       req.params.userId
+//     )
+//       .then(matches => {
+//         const profiles = matches.map(async (match) => {
+//           const userInfo = await UserService.getUserInfo(
+//             req.app.get('db'),
+//             match.match_user_id
+//           )
+//             .then(profile => {
+//               return profile;
+//             });
+            
+//           return {...userInfo};
+//         });
+//         Promise.all(profiles).then(profiles => res.json(UserService.serializeProfiles(profiles)));
+//       })
+//       .catch(next);
 
   async function checkUserExists(req, res, next) {
     try {
