1ad8dc494b36fabbfb389a4a863b724f3c419bce
Ron Martin <ronmartin89@gmail.com>
2020-04-30 13:38:27 -0700

    GET /matched/:userId now returns the matched user_id as well

--- src/matched/matched-service.js
+++ src/matched/matched-service.js
@@ -10,7 +10,7 @@ const MatchedService = {
   // TODO add GamerID when it gets added to the DB
   getUserInfo(db, user_id) {
     return db('user_info')
-      .select('display_name', 'bio', 'lfm_in', 'avatar')
+      .select('user_id', 'display_name', 'bio', 'lfm_in', 'avatar')
       .where({
         user_id
       })
@@ -33,17 +33,6 @@ const MatchedService = {
       .where({ user_id })
       .andWhere('match_user_id', user_id)
       .del();
-  },
-  // get all profile information for a user. We want data from user_genres, user_info, and user_platforms tables
-  getUserProfile(db, user_id){
-    // TODO add GamerID when it gets added to the DB
-    // return db
-    //   .select('user_info.display_name', 'user_info.bio', 'user_info.lfm_in', 'user_info.avatar', 'user_genres.genre', 'user_platforms.platform')
-    //   .from('users')
-    //   .innerJoin('user_info', 'users.id', 'user_info.user_id')
-    //   .innerJoin('user_genres', 'user_info.user_id', 'user_genres.user_id')
-    //   .innerJoin('user_platforms', 'user_genres.user_id', 'user_platforms.user_id')
-    //   .where('users.id', user_id);
   },
   // TODO
   serializeMatched(matched){}
