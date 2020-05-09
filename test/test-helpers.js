4968f3c4cc364d618a0cd0865e6339ee40629eea
Ron Martin <ronmartin89@gmail.com>
2020-05-09 12:25:17 -0700

    made rejections array

--- test/test-helpers.js
+++ test/test-helpers.js
@@ -82,11 +82,81 @@ function makeSeedUsersArray() {
     ];
 }
 /**
- * creates matches from the user_matches table
- * @returns {array} of user matches
+ * creates matches for the user_matches table
+ * @returns {array} of user matches objects
  */
 function makeMatchesArray() {
-    return [];
+    return [
+         {
+          user_id: 1,
+          match_user_id: 2
+        },
+        {
+          user_id: 1,
+          match_user_id: 4
+        },
+        {
+          user_id: 2,
+          match_user_id: 1
+        },
+        {
+          user_id: 2,
+          match_user_id: 5
+        },
+        {
+          user_id: 3,
+          match_user_id: 4
+        },
+        {
+          user_id: 4,
+          match_user_id: 3
+        },
+        {
+          user_id: 5,
+          match_user_id: 2
+        },
+        {
+          user_id: 6,
+          match_user_id: 5
+        }
+    ];
+}
+
+/**
+ * creates rejections for the user_rejections table
+ * @returns {array} of user rejection objects
+ */
+function makeRejectionsArray() {
+    return [
+        {
+            user_id: 1,
+            match_user_id: 3
+          },
+          {
+            user_id: 2,
+            match_user_id: 4
+          },
+          {
+            user_id: 2,
+            match_user_id: 6
+          },
+          {
+            user_id: 3,
+            match_user_id: 5
+          },
+          {
+            user_id: 4,
+            match_user_id: 1
+          },
+          {
+            user_id: 5,
+            match_user_id: 6
+          },
+          {
+            user_id: 6,
+            match_user_id: 2
+          }
+    ];
 }
 
 /**
